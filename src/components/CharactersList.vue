d6ac15c3e1521c4f0fbdeaa25c6089b40dad7852
mbknu <bouchelot.m@gmail.com>
2021-01-13 16:23:02 +0100

    render api, multiple character need to work on it

--- src/components/CharactersList.vue
+++ src/components/CharactersList.vue
@@ -1,5 +1,12 @@
 <template>
-  <h3>{{ characters.results }}</h3>
+  <div class="list" v-for="character in characters" :key="character.id">
+    <div class="details">
+      <!-- <img v-bind:src="`${character.image}`" class="profilpicture" /> -->
+      {{ character.name }}
+      {{ character.status }}
+      {{ character.species }} <br />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -15,14 +22,32 @@ export default {
   },
   methods: {
     fetchData() {
-      fetch(`https://rickandmortyapi.com/api/character/`)
+      function getRandom() {
+        const randomNumber = Math.floor(Math.random() * 672);
+        const randomNumber1 = Math.floor(Math.random() * 672);
+        const randomNumber2 = Math.floor(Math.random() * 672);
+        const randomNumber3 = Math.floor(Math.random() * 672);
+        const randomNumber4 = Math.floor(Math.random() * 672);
+        const randomNumber5 = Math.floor(Math.random() * 672);
+
+        const total = `${randomNumber}, ${randomNumber1}, ${randomNumber2}, ${randomNumber3}, ${randomNumber4}, ${randomNumber5}`;
+        return total;
+      }
+
+      fetch(`https://rickandmortyapi.com/api/character/[${getRandom()}]`)
         .then((response) => response.json())
         .then((data) => {
           this.characters = data;
+          // console.log("data", data);
         });
     },
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.list {
+  display: flex;
+  justify-content: space-evenly;
+}
+</style>
